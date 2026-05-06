439146226c3fb3fbee27dc0dde9e8e674c425537
Ramz001 <rkenjaev1@gmail.com>
2026-05-07 01:28:54 +0500

    feat: update version to 2.1.2, remove dateToTimestamp utility, and update exports in utils index

--- src/utils/date-to-timestamp.ts
+++
@@ -1,7 +0,0 @@
-export function dateToTimestamp(date?: Date | null) {
-	if (!date) return undefined
-	return {
-		seconds: Math.floor(date.getTime() / 1000),
-		nanos: (date.getTime() % 1000) * 1e6
-	}
-}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,2 @@
 export * from './convert-enum'
 export * from './grpc-exception'
-export * from './date-to-timestamp'
