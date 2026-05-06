4adc9d2f62029aa9f723c4d5a3b5c6abd00891d5
Ramz001 <rkenjaev1@gmail.com>
2026-05-06 22:06:29 +0500

    feat: update version to 2.0.12 and improve export structure in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from './utils/grpc-exception'
 export * from './utils/convert-enum'
 export * from './proto/paths'
-export * from './gen/auth'
-// @ts-expect-error - This file is generated and should exist, both files share same const protobufPackage
-export * from './gen/account'
+// Export protobuf generated code
+export * as account from './gen/account'
+export * as auth from './gen/auth'
