d175bb2be77ba4bbca455891af0a3315db36df35
Ramz001 <rkenjaev1@gmail.com>
2026-05-09 21:51:14 +0500

    feat: migration from protobufjs to buf to facilitate easier configuration and development

--- src/proto/paths.ts
+++ src/proto/paths.ts
@@ -1,6 +1,15 @@
 import { join } from 'path'
 
 export const PROTO_PATHS = {
-	AUTH: join(__dirname, '..', '..', 'proto', 'auth.proto'),
-	ACCOUNT: join(__dirname, '..', '..', 'proto', 'account.proto')
+	AUTH: join(__dirname, '..', '..', 'proto', 'auth', 'v1', 'auth.proto'),
+	ACCOUNT: join(
+		__dirname,
+		'..',
+		'..',
+		'proto',
+		'account',
+		'v1',
+		'account.proto'
+	),
+	COMMON: join(__dirname, '..', '..', 'proto', 'common', 'v1', 'common.proto')
 }
