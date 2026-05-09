ebc8ca92ce04d9c5f080734bf513b1aad86d9b53
Ramz001 <rkenjaev1@gmail.com>
2026-05-09 22:15:52 +0500

    feat: add new proto files for account and auth services, update paths and buf.yaml configuration

--- src/proto/paths.ts
+++ src/proto/paths.ts
@@ -1,15 +1,7 @@
 import { join } from 'path'
 
 export const PROTO_PATHS = {
-	AUTH: join(__dirname, '..', '..', 'proto', 'auth', 'v1', 'index.proto'),
-	ACCOUNT: join(
-		__dirname,
-		'..',
-		'..',
-		'proto',
-		'account',
-		'v1',
-		'index.proto'
-	),
-	COMMON: join(__dirname, '..', '..', 'proto', 'common', 'v1', 'index.proto')
+	AUTH: join(__dirname, '..', '..', 'proto', 'auth', 'v1.proto'),
+	ACCOUNT: join(__dirname, '..', '..', 'proto', 'account', 'v1.proto'),
+	COMMON: join(__dirname, '..', '..', 'proto', 'common', 'v1.proto')
 }
