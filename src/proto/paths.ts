4eea481261b3c8602bd9f062c594cce01df872cb
Ramz001 <rkenjaev1@gmail.com>
2026-05-09 22:03:34 +0500

    fix: fixing imports paths

--- src/proto/paths.ts
+++ src/proto/paths.ts
@@ -1,7 +1,7 @@
 import { join } from 'path'
 
 export const PROTO_PATHS = {
-	AUTH: join(__dirname, '..', '..', 'proto', 'auth', 'v1', 'auth.proto'),
+	AUTH: join(__dirname, '..', '..', 'proto', 'auth', 'v1', 'index.proto'),
 	ACCOUNT: join(
 		__dirname,
 		'..',
@@ -9,7 +9,7 @@ export const PROTO_PATHS = {
 		'proto',
 		'account',
 		'v1',
-		'account.proto'
+		'index.proto'
 	),
-	COMMON: join(__dirname, '..', '..', 'proto', 'common', 'v1', 'common.proto')
+	COMMON: join(__dirname, '..', '..', 'proto', 'common', 'v1', 'index.proto')
 }
