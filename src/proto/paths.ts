c814205cde806e911e8980cc52591e645b9d99d6
Ramz001 <rkenjaev1@gmail.com>
2026-05-12 18:22:16 +0500

    fix: update version to 3.1.1 in package.json and add PROTO_ROOT constant in paths.ts

--- src/proto/paths.ts
+++ src/proto/paths.ts
@@ -1,5 +1,7 @@
 import { join } from 'path'
 
+export const PROTO_ROOT = join(__dirname, '..', '..', 'proto')
+
 export const PROTO_PATHS = {
 	AUTH: join(__dirname, '..', '..', 'proto', 'auth', 'v1.proto'),
 	ACCOUNT: join(__dirname, '..', '..', 'proto', 'account', 'v1.proto'),
